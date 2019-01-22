3b0ab0f3fe79744b37d24b79963fca6f85055b69
licoyqtc <lico@yqtc.co>
2019-01-22 15:07:13 +0800

    commit

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/ubbey/ubox.uappsdk"
 	"log"
 	"yqtc.com/ubox.uapp/uvm/sdk/echo"
 )
@@ -16,10 +18,29 @@ func main() {
 	e := echo.New()
 
 	e.POST("/uapp/com.pvr.www/greet", func(context echo.Context) error {
-		return context.JSONPretty(200, "hi i am post pvr...", "")
+		return context.JSONPretty(200, "hi i am post pvr...\n", "")
 	})
 	e.GET("/uapp/com.pvr.www/greet", func(context echo.Context) error {
-		return context.JSONPretty(200, "hi i am get pvr...", "")
+		return context.JSONPretty(200, "hi i am get pvr...\n", "")
+	})
+
+	e.POST("/uapp/com.pvr.www/download", func(context echo.Context) error {
+		req := ubox_uappsdk.Sdk_downloader_task_download_req{}
+		req.Business = ubox_uappsdk.UAPP_PVR
+		req.Subbusiness = "test"
+		req.Method = "GET"
+		req.Dir = "/usr/local/application/com.pvr.www/workroot/data"
+		req.Url = "http://iamtest.yqtc.co/Application/com.pvr.www/pvr-1.0.0.xml"
+		req.Packname = "pvr-1.0.0.xml"
+
+		rsp, err := ubox_uappsdk.Sdk_downloader_task_download(req)
+		if err != nil {
+			log.Println(err.Error())
+			return context.JSONPretty(200, err.Error()+"\n", "")
+		}
+
+		data, _ := json.Marshal(rsp)
+		return context.JSONPretty(200, string(data)+"\n", "")
 	})
 
 	if true {
